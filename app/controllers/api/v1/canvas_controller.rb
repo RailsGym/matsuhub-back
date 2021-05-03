346bf4d8252f759bc47f6127dfc527fa24a0257f
noria_t <n61.takeuchi.nozomi@gmail.com>
2021-05-03 16:21:01 +0900

    WIP

--- app/controllers/api/v1/canvas_controller.rb
+++ app/controllers/api/v1/canvas_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::CanvasController < Api::V1::ApplicationController
+
+  def create
+    @canvas = Canvas.new(canvas_params)
+    owner_id = current_user.id
+    if @canvas.save!
+      @canvas.assign_attributes(owner_id: owner_id)
+      render json: { canvas: @canvas, errors: [] }
+    else
+      render json: { errors: @canvas.errors.full_messages }
+    end
+  end
+
+  private
+
+  def canvas_params
+    params.require(:canvas).permit(:title)
+  end
 end
