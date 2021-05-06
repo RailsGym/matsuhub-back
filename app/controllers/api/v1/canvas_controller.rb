7866ee294554b307afea2c8753e035d39f1cc5d5
michiwo1 <mk1104kado@gmail.com>
2021-05-06 09:37:04 +0900

    キャンバスを開いた時の情報を渡すapiを作成

--- app/controllers/api/v1/canvas_controller.rb
+++ app/controllers/api/v1/canvas_controller.rb
@@ -3,4 +3,25 @@ class Api::V1::CanvasController < Api::V1::ApplicationController
     canvas = Canvas.where(owner_id: current_user.id)
     render json: {canvases: canvas}
   end
-end
+
+  def show
+    canvas = Canvas.find(params[:id])
+    labels = canvas.labels
+    areas = 
+      areas_array = []
+      Area.all.each do |area|
+        if labels.where(area_id: area.id).present?
+          labels_array = []
+          labels.where(area_id: area.id).each do |label|
+            labels_array << {id: label.id, title: label.title, position: label.position}
+          end
+          areas_array << [id: area.id, area_type: area.area_type, area_type_text: area.area_type_text, description: area.description, label: labels_array]
+        else
+          areas_array << [id: area.id, area_type: area.area_type, description: area.description, label: []]
+        end
+      end
+      
+    canvas_area_label = {title: canvas.title, area: areas}
+    render json: {canvas: canvas_area_label}
+  end
+end 
