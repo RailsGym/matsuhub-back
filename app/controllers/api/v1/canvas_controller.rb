23bc5bb9889083e3d6ff4eda75a2e6a2873ff7f4
Matsushin <matsushin.dev@gmail.com>
2021-05-06 11:11:35 +0900

    リファクタ

--- app/controllers/api/v1/canvas_controller.rb
+++ app/controllers/api/v1/canvas_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CanvasController < Api::V1::ApplicationController
+  before_action :set_canvas, only: %i[show]
 
   def index
     canvas = Canvas.where(owner_id: current_user.id)
@@ -6,24 +7,14 @@ class Api::V1::CanvasController < Api::V1::ApplicationController
   end
 
   def show
-    canvas = Canvas.find(params[:id])
-    labels = canvas.labels
-    areas = 
-      areas_array = []
-      Area.all.each do |area|
-        if labels.where(area_id: area.id).present?
-          labels_array = []
-          labels.where(area_id: area.id).each do |label|
-            labels_array << {id: label.id, title: label.title, position: label.position}
-          end
-          areas_array << {id: area.id, area_type: area.area_type, area_type_text: area.area_type_text, description: area.description, label: labels_array}
-        else
-          areas_array << {id: area.id, area_type: area.area_type, description: area.description, label: []}
-        end
-      end
-      
-    canvas_area_label = {title: canvas.title, areas: areas}
-    render json: {canvas: canvas_area_label}
+    labels = @canvas.labels
+    areas = Area.all.map do |area|
+              area_label_hash = labels.where(area: area).map do |label|
+                { id: label.id, title: label.title, position: label.position }
+              end
+              { id: area.id, area_type: area.area_type, area_type_text: area.area_type_text, description: area.description, labels: area_label_hash }
+            end
+    render json: { canvas: { title: @canvas.title, areas: areas } }
   end
 
   def create
@@ -40,5 +31,9 @@ class Api::V1::CanvasController < Api::V1::ApplicationController
 
   def canvas_params
     params.require(:canvas).permit(:title)
+  end
+
+  def set_canvas
+    @canvas = Canvas.find(params[:id])
   end
 end
