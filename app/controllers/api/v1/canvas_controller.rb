5c21097ec90593d498485c4386eaae24ca09d2b9
michiwo1 <mk1104kado@gmail.com>
2021-05-06 10:51:59 +0900

    レビュー修正

--- app/controllers/api/v1/canvas_controller.rb
+++ app/controllers/api/v1/canvas_controller.rb
@@ -16,16 +16,16 @@ class Api::V1::CanvasController < Api::V1::ApplicationController
           labels.where(area_id: area.id).each do |label|
             labels_array << {id: label.id, title: label.title, position: label.position}
           end
-          areas_array << [id: area.id, area_type: area.area_type, area_type_text: area.area_type_text, description: area.description, label: labels_array]
+          areas_array << {id: area.id, area_type: area.area_type, area_type_text: area.area_type_text, description: area.description, label: labels_array}
         else
-          areas_array << [id: area.id, area_type: area.area_type, description: area.description, label: []]
+          areas_array << {id: area.id, area_type: area.area_type, description: area.description, label: []}
         end
       end
       
-    canvas_area_label = {title: canvas.title, area: areas}
+    canvas_area_label = {title: canvas.title, areas: areas}
     render json: {canvas: canvas_area_label}
   end
-  
+
   def create
     @canvas = Canvas.new(canvas_params)
     @canvas.owner = current_user
